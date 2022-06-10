3135546eeb94fa55b639ccf5870762de6a59fc5c
Matt Howard <matthoward30740@gmail.com>
2022-06-10 18:53:12 -0400

    Add project files.

---
+++ MonitorOff/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace MonitorOff
+{
+    public partial class Form1 : Form
+    {
+        public int WM_SYSCOMMAND = 0x0112;
+        public int SC_MONITORPOWER = 0xF170;
+
+        [DllImport("user32.dll")]
+        private static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+
+            SendMessage(this.Handle.ToInt32(), WM_SYSCOMMAND, SC_MONITORPOWER, 2);
+        }
+    }      
+
+   
+}
+
+
